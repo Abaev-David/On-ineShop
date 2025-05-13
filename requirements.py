e73909382277176f3a3d7cfe5e4e9ea3b63fecf5
Abaev-David <abaevdavid693@gmail.com>
2025-05-13 07:22:45 +0300

    Create requirements.py
    Список товаров

---
+++ requirements.py
@@ -0,0 +1,38 @@
+products_list = [
+    {
+        'name': 'Iphone 16',
+        'description': 'Айфончик 16',
+        'price': 899.99,
+        'image_file': 'Iphone 16_img.jpg'
+    },
+    {
+        'name': 'Iphone 16 Pro',
+        'description': 'Крутой айфончик 16',
+        'price': 1099.99,
+        'image_file': 'Iphone 16 Pro_img.jpg'
+    },
+    {
+        'name': 'Iphone 16 Plus',
+        'description': 'Большой айфончик 16',
+        'price': 999.99,
+        'image_file': 'Iphone 16 Plus_img.jpg'
+    },
+    {
+        'name': 'Iphone 16 Pro Max',
+        'description': 'Крутой большой айфончик 16',
+        'price': 1299.99,
+        'image_file': 'Iphone 16 Pro Max_img.jpg'
+    },
+    {
+        'name': 'Samsung Galaxy A55',
+        'description': 'Телефон нормального человека',
+        'price': 399.99,
+        'image_file': 'Samsung Galaxy A55_img.jpg'
+    },
+    {
+        'name': 'Samsung Galaxy Tab S10',
+        'description': 'Планшет нормального человека',
+        'price': 999.99,
+        'image_file': 'Samsung Galaxy Tab S10_img.jpg'
+    }
+]
